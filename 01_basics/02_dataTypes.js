f87793e673898e63f29055116fca40a996bf6183
Nowshen Anjuman Khan Prapte <57893389+nowshen-khan@users.noreply.github.com>
2023-12-24 04:41:56 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodjs, not browser
+
+console.log(3+3); 
+console.log("hitesh");
+
+let name = "hitesh"
+let age= 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
