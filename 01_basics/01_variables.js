fbfa926ea61f7ba4fc49ea215b1f28c265976e0c
Nowshen Anjuman Khan Prapte <57893389+nowshen-khan@users.noreply.github.com>
2023-12-24 04:42:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "nowshen@gmail.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+let accountState;  
+
+// accountId = 2
+accountEmail = "aregeag"
+accountPassword = "gsrre"
+accountCity = "adbhr"
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope 
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity])
